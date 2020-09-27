8c610c7deb4220bc34e11f8b7594dbfb2c17c230
BROCHARD, Xavier Alexandre <xavier-alexandre.brochard@airbus.com>
2020-09-27 23:48:07 +0200

    Update KafkaProducerApp.java

--- src/main/java/KafkaProducerApp.java
+++ src/main/java/KafkaProducerApp.java
@@ -1,6 +1,8 @@
 import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
 public class KafkaProducerApp {
   /**
@@ -10,11 +12,23 @@ public class KafkaProducerApp {
    */
   public static void main(String[] args) {
     Properties props = new Properties();
-    props.put("bootstrap.servers", "BROKER-1:9092, BROKER-2:9093");
+    props.put("bootstrap.servers", "localhost:9092, localhost:9093, localhost:9094");
+    props.put("acks", "all");
     props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
     props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-    KafkaProducer myfProducer = new KafkaProducer(props);
+    Producer<String, String> myProducer = new KafkaProducer<>(props);
+    String topic = "lordoftherings";
+    try (myProducer) {
+      for (int i = 0; i < 150; i++) {
+        String key = Integer.toString(i);
+        String value = "My Message " + Integer.toString(i);
+        ProducerRecord<String, String> myRecord = new ProducerRecord<>(topic, key, value);
+        myProducer.send(myRecord).get();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
 }
