4b9d353657fd88f7a3c14dd444cecf656bf1ffac
BROCHARD, Xavier Alexandre <xavier-alexandre.brochard@airbus.com>
2020-10-07 15:57:07 +0200

    Continue the training

---
+++ src/main/java/KafkaConsumerApp.java
@@ -0,0 +1,42 @@
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+public class KafkaConsumerApp {
+
+  /**
+   * Main.
+   * 
+   * @param args Arguments on the method
+   */
+  public static void main(String[] args) {
+    Properties props = new Properties();
+    props.put("bootstrap.servers", "localhost:9092");
+    props.put("acks", "all");
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+    Consumer<String, String> myConsumer = new KafkaConsumer<>(props);
+    ArrayList<String> topics = new ArrayList<>();
+    topics.add("lordoftherings");
+
+    try (myConsumer) {
+      myConsumer.subscribe(topics);
+      while (true) {
+        ConsumerRecords<String, String> records = myConsumer.poll(Duration.ofSeconds(100));
+        for (ConsumerRecord<String, String> record : records) {
+          System.out.println(String.format("Topic: %s, Partiion: %d, Offset: %d, Key: %s, Value: %s", record.topic(),
+              record.partition(), record.offset(), record.value()));
+        }
+      }
+    } catch (Exception e) {
+      System.out.println(e.getStackTrace());
+    }
+  }
+
+}
