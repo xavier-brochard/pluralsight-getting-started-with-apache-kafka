a3dedc21b011d34135052e8967b492cc9dbe3b06
BROCHARD, Xavier Alexandre <xavier-alexandre.brochard@airbus.com>
2020-10-10 18:25:35 +0200

    Format

--- src/main/java/KafkaConsumerApp.java
+++ src/main/java/KafkaConsumerApp.java
@@ -2,41 +2,43 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Properties;
 
-import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 public class KafkaConsumerApp {
 
-  /**
-   * Main.
-   * 
-   * @param args Arguments on the method
-   */
-  public static void main(String[] args) {
-    Properties props = new Properties();
-    props.put("bootstrap.servers", "localhost:9092");
-    props.put("acks", "all");
-    props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+    /**
+     * Main.
+     * 
+     * @param args Arguments on the method
+     */
+    public static void main(String[] args) {
+        Properties props = new Properties();
 
-    Consumer<String, String> myConsumer = new KafkaConsumer<>(props);
-    ArrayList<String> topics = new ArrayList<>();
-    topics.add("lordoftherings");
+        props.setProperty("bootstrap.servers", "localhost:9092");
+        props.setProperty("group.id", "test");
+        props.setProperty("enable.auto.commit", "true");
+        props.setProperty("auto.commit.interval.ms", "1000");
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
-    try (myConsumer) {
-      myConsumer.subscribe(topics);
-      while (true) {
-        ConsumerRecords<String, String> records = myConsumer.poll(Duration.ofSeconds(100));
-        for (ConsumerRecord<String, String> record : records) {
-          System.out.println(String.format("Topic: %s, Partiion: %d, Offset: %d, Key: %s, Value: %s", record.topic(),
-              record.partition(), record.offset(), record.value()));
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        ArrayList<String> topics = new ArrayList<>();
+        topics.add("harrypotter");
+
+        try (consumer) {
+            consumer.subscribe(topics);
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.println(String.format("Topic: %s, Partition: %d, Offset: %d, Key: %s, Value: %s",
+                            record.topic(), record.partition(), record.offset(), record.key(), record.value()));
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-      }
-    } catch (Exception e) {
-      System.out.println(e.getStackTrace());
     }
-  }
 
 }
--- src/main/java/KafkaProducerApp.java
+++ src/main/java/KafkaProducerApp.java
@@ -5,30 +5,29 @@ import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
 public class KafkaProducerApp {
-  /**
-   * Main.
-   * 
-   * @param args Arguments on the method
-   */
-  public static void main(String[] args) {
-    Properties props = new Properties();
-    props.put("bootstrap.servers", "localhost:9092, localhost:9093, localhost:9094");
-    props.put("acks", "all");
-    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    /**
+     * Main.
+     * 
+     * @param args Arguments on the method
+     */
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-    Producer<String, String> myProducer = new KafkaProducer<>(props);
-    String topic = "lordoftherings";
-    try (myProducer) {
-      for (int i = 0; i < 150; i++) {
-        String key = Integer.toString(i);
-        String value = "My Message " + Integer.toString(i);
-        ProducerRecord<String, String> myRecord = new ProducerRecord<>(topic, key, value);
-        myProducer.send(myRecord).get();
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
+        Producer<String, String> myProducer = new KafkaProducer<>(props);
+        String topic = "harrypotter";
+        try (myProducer) {
+            for (int i = 0; i < 150; i++) {
+                String key = Integer.toString(i);
+                String value = "My Message " + Integer.toString(i);
+                ProducerRecord<String, String> myRecord = new ProducerRecord<>(topic, key, value);
+                myProducer.send(myRecord).get();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
-  }
 
 }
